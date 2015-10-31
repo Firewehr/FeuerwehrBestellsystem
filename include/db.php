d5aabf25502811e082a298a57a6f4a10544bcf8d
mathias <mathias@mathias-ThinkPad-T440s>
2015-10-31 08:01:12 +0100

    First Commit

---
+++ include/db.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+  $hostname = 'localhost';
+  $username = 'pos';
+  $password = 'pos';
+  $dbname = 'pos';
+ */
+
+// Create connection
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
