e062cc9344835903c9b9cf621318d835b8307710
mathias <mathias@mathias-ThinkPad-T440s>
2016-06-05 11:52:31 +0200

    remove sql statements

--- BestellungBezahlt.php
+++ BestellungBezahlt.php
@@ -1,22 +1,27 @@
 <?php
+
 require_once('auth.php');
 
-$sql = $_GET['rowid'];
-
-
-$sql = substr($sql, 0, -10);
 
 require_once 'include/db.php';
 
 
-$sql = "UPDATE `bestellungen` SET `timestampBezahlung`=current_timestamp, `kueche`='1', `kellnerZahlung`='" . htmlspecialchars($_SESSION['user']['username']) . "' WHERE rowid=" . $sql;
+$myArray = $_REQUEST['listePositionen'];
 
 
-//echo $sql;
-if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . mysqli_error($conn);
+foreach ($myArray as $row) {
+
+    $sql = "UPDATE `bestellungen` "
+            . "SET `timestampBezahlung`=current_timestamp,"
+            . "`kueche`='1',"
+            . "`kellnerZahlung`='" . htmlspecialchars($_SESSION['user']['username']) . "' "
+            . "WHERE rowid=" . intval($row);
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
 }
 
 mysqli_close($conn);
