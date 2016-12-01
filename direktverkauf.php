87670b24f976d0f97da491419d1e4ec1278ff6fe
mathias <mathias@mathias-ThinkPad-T440s>
2016-12-01 09:04:02 +0100

    code cleaned

--- direktverkauf.php
+++ direktverkauf.php
@@ -13,25 +13,21 @@ echo '</div>';
 ?>
 <div data-role="content" id="BestellungenDirektverkauf">
 
-    <div data-role="popup" id="popupDialog2" data-overlay-theme="a" data-theme="a" data-dismissible="false" style="max-width:400px;">
-            <div data-role="header" data-theme="a">
-                <h1>Beilagen</h1>
-        </div>
-            <div role="main" class="ui-content" >
-
-            <div id="popupDialog2Content">
-
-            </div>
-                    <a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-btn-b" onclick="updateZusatzinfo();" data-transition="flow">OK</a>
-                </div>
-    </div>
-
     <div data-role="tabs" id='TischAnzeigenDirektverkauf' title="alert-tab">
             <div data-role="navbar">
             <ul>
                 <?php
-                echo '<li><a id="tabGetraenkeDirektverkauf" class="ui-btn-active ui-state-persist" href="listGetraenke_direktverkauf.php?tischnummer=' . $Tischnummer . '" data-theme="a" data-ajax="false">Getränke</a></li>';
-                echo '<li><a id="tabSpeisenDirektverkauf" class="ui-btn" href="listSpeisen_direktverkauf.php?tischnummer=' . $Tischnummer . '" data-theme="a" data-ajax="false">Speisen</a></li>';
+                echo '<li>'
+                . '<a id="tabGetraenkeDirektverkauf" '
+                        . 'class="ui-btn-active ui-state-persist" '
+                        . 'href="listGetraenke_direktverkauf.php?tischnummer=' . $Tischnummer . '" '
+                        . 'data-theme="a" data-ajax="false">Getränke'
+                        . '</a></li>';
+                echo '<li>'
+                . '<a id="tabSpeisenDirektverkauf" class="ui-btn" '
+                        . 'href="listSpeisen_direktverkauf.php?tischnummer=' . $Tischnummer . '" '
+                        . 'data-theme="a" data-ajax="false">Speisen'
+                        . '</a></li>';
                 ?>                
             </ul>
         </div>
@@ -40,8 +36,9 @@ echo '</div>';
 
 
 <?php
+/*
 echo '</div>';
-echo '</div>';
+echo '</div>';*/
 
 echo '<div data-role="footer">';
 echo '</div>';
