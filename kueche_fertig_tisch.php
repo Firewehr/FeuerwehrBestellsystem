e200e2c5ab25f783c83945830290ab1be4e1a6ca
mathias <mathias@mathias-ThinkPad-T440s>
2016-12-01 09:05:01 +0100

    code cleaned

--- kueche_fertig_tisch.php
+++ kueche_fertig_tisch.php
@@ -2,20 +2,36 @@
 
 require_once('auth.php');
 require_once 'include/db.php';
-include_once('include/db.php');
 
-$myArray = $_REQUEST['listePositionen'];
+$tischnummer = intval($_POST['tischnummer']);
+$PositionsListe = $_REQUEST['listePositionen'];
 
-var_dump($myArray);
-foreach ($myArray as $row) {
+$timestamp = date('Y-m-d G:i:s');
 
-    $sql = "UPDATE `bestellungen` SET `zeitKueche`=current_timestamp, `kueche`= '1' WHERE rowid=" . intval($row);
+foreach ($PositionsListe as $row) {
+
+    $sql = "UPDATE `bestellungen` SET "
+            . "`zeitKueche`=current_timestamp,"
+            . "`kueche`= '1',"
+            . "`print`=2 "
+            . "WHERE rowid=" . intval($row);
 
     if (mysqli_query($conn, $sql)) {
         echo "Record updated successfully";
     } else {
         echo "Error updating record: " . mysqli_error($conn);
     }
+
+//Insert Print
+    $sql = "INSERT INTO print (bestellungID,timestamp) VALUES (" . intval($row) . ",'" . $timestamp . "')";
+    echo $sql;
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
 }
+
+
 
 mysqli_close($conn);
