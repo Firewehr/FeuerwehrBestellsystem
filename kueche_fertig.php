b68cfa411f95d32e2a4236398880efc6e020b05e
mathias <mathias@mathias-ThinkPad-T440s>
2016-12-01 09:04:51 +0100

    code cleaned

--- kueche_fertig.php
+++ kueche_fertig.php
@@ -1,20 +1,27 @@
 <?php
+
 require_once('auth.php');
 
 require_once 'include/db.php';
 
-$myArray = $_REQUEST['listePositionen'];
+$myArray = $_REQUEST['rowid'];
 
+/*
+  foreach ($myArray as $row) {
 
-foreach ($myArray as $row) {
+  } */
 
-    $sql = "UPDATE `bestellungen` SET `zeitKueche`=current_timestamp, `kueche`= '1' WHERE rowid=" . intval($row);
+$sql = "UPDATE `bestellungen` "
+        . "SET `zeitKueche`=current_timestamp,"
+        . "`print`='2',"
+        . "`kueche`='1' "
+        . "WHERE rowid=" . intval($_GET['rowid']);
 
-    if (mysqli_query($conn, $sql)) {
-        echo "Record updated successfully";
-    } else {
-        echo "Error updating record: " . mysqli_error($conn);
-    }
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
 }
 
+echo $sql;
 mysqli_close($conn);
