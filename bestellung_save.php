689d7c328a187935879f42fd84d5fd6ba2d7d1f4
mathias <mathias@mathias-ThinkPad-T440s>
2017-07-19 09:17:25 +0200

    cosmetics

--- bestellung_save.php
+++ bestellung_save.php
@@ -14,7 +14,7 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-if($kuechefertig=="1") {
+if ($kuechefertig == "1") {
     $sql = "INSERT `bestellungen` SET `tischnummer`='$tischnummer',`position`=$positionsid, `timestampBestellung`=now(),`zeitKueche`=now(),kueche=1, `kellner`='" . htmlspecialchars($_SESSION['user']['username']) . "'";
 } else {
     $sql = "INSERT `bestellungen` SET `tischnummer`='$tischnummer',`position`=$positionsid, `timestampBestellung`=now(), `kellner`='" . htmlspecialchars($_SESSION['user']['username']) . "'";
