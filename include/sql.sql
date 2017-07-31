1aad3daf2ce20b446e27faf414cdc5a03da3f632
mathias <mathias@mathias-ThinkPad-T440s>
2017-07-31 07:35:06 +0200

    database structure changes

--- include/sql.sql
+++ include/sql.sql
@@ -1,17 +1,34 @@
-
-SQL Structure:
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
 --
-
+-- Host: mysqlsvr11.world4you.com
+-- Erstellungszeit: 31. Jul 2017 um 07:31
+-- Server-Version: 5.1.73
+-- PHP-Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE IF NOT EXISTS `beilagen` (
+--
+-- Datenbank: `ff_manhartsbrunnatdb2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `beilagen`
+--
+
+CREATE TABLE `beilagen` (
   `rowid` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `name` varchar(255) NOT NULL,
@@ -25,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `beilagen` (
 -- Tabellenstruktur für Tabelle `bestellungen`
 --
 
-CREATE TABLE IF NOT EXISTS `bestellungen` (
+CREATE TABLE `bestellungen` (
   `rowid` int(11) NOT NULL,
   `zeitstempel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `position` int(11) NOT NULL,
@@ -51,36 +68,29 @@ CREATE TABLE IF NOT EXISTS `bestellungen` (
 -- Tabellenstruktur für Tabelle `positionen`
 --
 
-CREATE TABLE IF NOT EXISTS `positionen` (
+CREATE TABLE `positionen` (
   `rowid` int(11) NOT NULL,
   `Positionsname` text CHARACTER SET latin1 NOT NULL,
   `Betrag` decimal(10,2) NOT NULL,
   `type` int(2) NOT NULL,
-  `Kurzbezeichnung` varchar(30) COLLATE latin1_german1_ci NOT NULL,
+  `Kurzbezeichnung` varchar(30) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL,
   `maxBestellbar` int(11) NOT NULL DEFAULT '-1',
   `reihenfolge` int(11) NOT NULL,
-  `color` varchar(6) COLLATE latin1_german1_ci NOT NULL,
-  `icon` varchar(30) COLLATE latin1_german1_ci NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+  `color` varchar(8) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL,
+  `icon` varchar(30) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `tabelle`
+-- Tabellenstruktur für Tabelle `print`
 --
 
-CREATE TABLE IF NOT EXISTS `tabelle` (
-  `row_id` int(6) NOT NULL,
-  `temp` float NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `value` int(4) NOT NULL,
-  `device` varchar(4) NOT NULL,
-  `min` int(4) NOT NULL,
-  `max` int(4) NOT NULL,
-  `licht` int(11) NOT NULL,
-  `minlicht` int(11) NOT NULL,
-  `maxlicht` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `print` (
+  `rowid` int(11) NOT NULL,
+  `bestellungID` int(11) NOT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -88,12 +98,14 @@ CREATE TABLE IF NOT EXISTS `tabelle` (
 -- Tabellenstruktur für Tabelle `tische`
 --
 
-CREATE TABLE IF NOT EXISTS `tische` (
-  `rowid` int(11) NOT NULL,
+CREATE TABLE `tische` (
   `tischnummer` int(11) NOT NULL,
   `tischname` varchar(7) NOT NULL,
   `zeitstempel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `kellenr` int(11) NOT NULL
+  `userID` int(11) NOT NULL,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `color` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -102,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `tische` (
 -- Tabellenstruktur für Tabelle `type`
 --
 
-CREATE TABLE IF NOT EXISTS `type` (
+CREATE TABLE `type` (
   `rowid` int(2) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -110,27 +122,15 @@ CREATE TABLE IF NOT EXISTS `type` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `rowid` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `passwordhash` varchar(60) NOT NULL,
-  `unused` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Tabellenstruktur für Tabelle `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `admin` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -161,31 +161,24 @@ ALTER TABLE `positionen`
   ADD KEY `type` (`type`);
 
 --
--- Indizes für die Tabelle `tabelle`
+-- Indizes für die Tabelle `print`
 --
-ALTER TABLE `tabelle`
-  ADD PRIMARY KEY (`row_id`);
+ALTER TABLE `print`
+  ADD PRIMARY KEY (`rowid`);
 
 --
 -- Indizes für die Tabelle `tische`
 --
 ALTER TABLE `tische`
-  ADD PRIMARY KEY (`rowid`),
-  ADD UNIQUE KEY `tischname` (`tischname`),
-  ADD KEY `tischnummer` (`tischnummer`);
+  ADD PRIMARY KEY (`tischnummer`),
+  ADD UNIQUE KEY `tischnummer` (`tischnummer`),
+  ADD KEY `tischnummer_2` (`tischnummer`);
 
 --
 -- Indizes für die Tabelle `type`
 --
 ALTER TABLE `type`
   ADD PRIMARY KEY (`rowid`);
-
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`rowid`),
-  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -202,37 +195,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `beilagen`
 --
 ALTER TABLE `beilagen`
-  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT für Tabelle `bestellungen`
 --
 ALTER TABLE `bestellungen`
-  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT für Tabelle `positionen`
 --
 ALTER TABLE `positionen`
-  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 --
--- AUTO_INCREMENT für Tabelle `tabelle`
+-- AUTO_INCREMENT für Tabelle `print`
 --
-ALTER TABLE `tabelle`
-  MODIFY `row_id` int(6) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `print`
+  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT für Tabelle `tische`
 --
 ALTER TABLE `tische`
-  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
-  MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tischnummer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100012;
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;COMMIT;
+
+
+INSERT INTO `users` (`id`, `username`, `password`, `timestamp`, `admin`) VALUES
+(1, 'admin', '$2a$10$w8aXzAADK9NvQN2FRGQ.9.TLwZChoT4psKE4OAApg5XDIIJz2kSGS', '2015-09-03 15:15:07', 1);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
