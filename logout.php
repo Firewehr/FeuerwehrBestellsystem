46866d6121520cf18142593b9ea03f630fee7331
Skopy77 <31206081+Skopy77@users.noreply.github.com>
2017-08-24 21:13:26 +0200

    fixed redirect to index.php
    session_unset();
    header('Location: index.php');

--- logout.php
+++ logout.php
@@ -9,6 +9,6 @@ if (ini_get('session.use_cookies')) {
             session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure"'], $params['httponly']
     );
 }
-session_destroy();
-header('Location: ./login.php');
+session_unset();
+header('Location: index.php');
 ?>
