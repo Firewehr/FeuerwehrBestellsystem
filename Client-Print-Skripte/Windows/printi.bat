c1b3d7c40d4a5c30644430aac7125db32a6c39c7
Skopy77 <31206081+Skopy77@users.noreply.github.com>
2017-08-27 12:32:20 +0200

    Add files via upload
    Windows POS Printer Skripte

---
+++ Client-Print-Skripte/Windows/printi.bat
@@ -0,0 +1,6 @@
+@echo off
+:: Batch-Skript mit dem man über freigegebene Drucker ansprechen kann. Der SMB-Drucker heißt "Receipt Printer" und die Datei ist die printPOS.txt. Anleitung zur Freigabe & Treibereinstellungen unter "https://mike42.me/blog/2015-04-getting-a-usb-receipt-printer-working-on-windows"
+
+print /D:"\\%COMPUTERNAME%\Receipt Printer" printPOS.txt
+
+
