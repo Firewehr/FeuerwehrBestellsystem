8f0208180a68db74caaadd3ecaa87cff63e2977e
Skopy77 <31206081+Skopy77@users.noreply.github.com>
2018-07-07 00:26:19 +0200

    Update printi.bat

--- Client-Print-Skripte/Windows/printi.bat
+++ Client-Print-Skripte/Windows/printi.bat
@@ -1,5 +1,4 @@
 @echo off
-:: Batch-Skript mit dem man über freigegebene Drucker ansprechen kann. Der SMB-Drucker heißt "Receipt Printer" und die Datei ist die printPOS.txt. Anleitung zur Freigabe & Treibereinstellungen unter "https://mike42.me/blog/2015-04-getting-a-usb-receipt-printer-working-on-windows"
+:: Batch-Skript mit dem man Ã¼ber freigegebene Drucker ansprechen kann. Der SMB-Drucker heiÃŸt "Receipt Printer" und die Datei ist die printPOS.txt. Anleitung zur Freigabe & Treibereinstellungen unter "https://mike42.me/blog/2015-04-getting-a-usb-receipt-printer-working-on-windows"
 
-print /D:"\\%COMPUTERNAME%\Receipt Printer" printPOS.txt
-
+print /D:"\\%COMPUTERNAME%\Receipt Printer" C:\POS-Daten\printPOS.txt
