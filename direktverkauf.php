6d1e4574ad6c36475b154b6124d6eaf821113043
Skopy77 <31206081+Skopy77@users.noreply.github.com>
2018-08-31 10:05:55 +0200

    Add History/Storno AND Reset

--- direktverkauf.php
+++ direktverkauf.php
@@ -1,16 +1,19 @@
 <?php
 require_once('auth.php');
 error_reporting(E_ALL);
-
-
+include_once('include/db.php');
 header("Cache-Control: no-cache, must-revalidate");
 $Tischnummer = 999999;
 include_once ("include/db.php");
 echo '<div data-role="header" data-position="fixed">';
 echo '<h1>Direktverkauf</h1>';
 echo '<a href="#indexPage" class="ui-btn ui-btn-left ui-icon-arrow-l ui-btn-icon-left">Menü</a>';
+echo '<a href="#DirektHistory" onclick="DirektHistory();" class="ui-btn-right">Historie</a>';
 echo '</div>';
 ?>
+
+<button onclick="Direkt_reset();" class="ui-btn ui-btn-inline ui-icon-delete ui-btn-icon-left">Reset</button>
+
 <div data-role="content" id="BestellungenDirektverkauf">
 
     <div data-role="tabs" id='TischAnzeigenDirektverkauf' title="alert-tab">
@@ -28,6 +31,13 @@ echo '</div>';
                         . 'href="listSpeisen_direktverkauf.php?tischnummer=' . $Tischnummer . '" '
                         . 'data-theme="a" data-ajax="false">Speisen'
                         . '</a></li>';
+				/*
+				echo '<li>'
+                . '<a id="tabKaffeeDirektverkauf" class="ui-btn" '
+                        . 'href="listKaffee_direktverkauf.php?tischnummer=' . $Tischnummer . '" '
+                        . 'data-theme="a" data-ajax="false">Kaffee/Kuchen'
+                        . '</a></li>';
+				*/
                 ?>                
             </ul>
         </div>
