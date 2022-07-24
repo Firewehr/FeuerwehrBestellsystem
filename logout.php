7088ade32e672dfd71f20d99c9ef4dbacc0d7f78
Skopy77 <31206081+Skopy77@users.noreply.github.com>
2022-07-24 20:57:01 +0200

    Update logout.php
    adding comments

--- logout.php
+++ logout.php
@@ -1,14 +1,22 @@
 <?php
 
 error_reporting(E_ALL);
+
+// Initialize the session
 session_start();
+
+// Unset all of the session variables
 $_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
 if (ini_get('session.use_cookies')) {
     $params = session_get_cookie_params();
     setcookie(
             session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']
     );
 }
+// Destroy the session.
 session_destroy();
 header('Location: ./login.php');
 ?>
